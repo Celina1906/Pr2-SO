a7e2eca3b52e8460a446170b65f9c9062457cd08
María José Porras <marijopm27@estudiantec.cr>
2023-10-15 22:35:08 -0600

    Update Prueba1
    - create
    - list
    - verbose (simple)

--- Prueba1.c
+++ Prueba1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_FILENAME_LENGTH 256
 
@@ -14,36 +15,88 @@ struct File {
     long end;
 };
 
-struct Archive {
+struct PackageInfo {
     struct File files[MAX_FILE_COUNT];
     size_t file_count;
     // conteo de espacios libres
     // pos inicio y fin de los espacios
 };
 
-void crear(const char* package_name) {
-    FILE *archivo = fopen(package_name, "wb"); // Abrir el archivo en modo escritura binaria
+bool verbose_flag = false;
 
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo");
-        return;
+void verbose(char* reports[], int numReports) {
+    for (int i = 0; i < numReports; i++) {
+        printf("%s\n", reports[i]);
     }
-
-    struct Archive archive;
-    archive.file_count = 0;
-    
-    // Escribir la estructura Archive en el archivo
-    size_t escritos = fwrite(&archive, sizeof(struct Archive), 1, archivo);
-
-    if (escritos != 1) {
-        perror("Error al escribir en el archivo");
-    }
-
-    fclose(archivo);
-
 }
 
-void recuperar_info(struct Archive *empacado, const char* package_name){
+void crear(const char* package_name,  char** files, int file_count) {
+    char* reports[3]; // Array de punteros a cadenas para informes
+
+    reports[0] = "Se creó el paquete de salida";
+    reports[1] = "Se escribieron los archivos siguientes:";
+    reports[2] = "Se terminó de escribir el paquete";
+    FILE* package = fopen(package_name, "wb");
+    if (package == NULL) {
+        perror("No se pudo crear el archivo empacado");
+        exit(1);
+    }
+    
+    struct PackageInfo package_info;
+    package_info.file_count = 0; //inicializamos en 0 --> aumenta cuenta por cada file que se escriba en el paquete
+    long current_byte = sizeof(struct PackageInfo);
+
+    for (int i = 0; i < file_count; i++) {
+        FILE* file = fopen(files[i], "rb");
+        if (file == NULL) {
+            perror("No se pudo abrir el archivo");
+            exit(1);
+        }
+
+        struct File add_file; // Cada file a empacar lleva su struct
+        add_file.start = current_byte; // inicializa segun el current byte - 1. si va empezando es 0 sino se actualiza a ultima posicion
+
+        fseek(file, 0, SEEK_END);
+        add_file.end = add_file.start + ftell(file); 
+        current_byte = add_file.end;
+
+        rewind(file); // Reinicia el puntero del file
+
+        strncpy(add_file.name, files[i], MAX_FILENAME_LENGTH);
+        add_file.size = ftell(file);
+
+        // Agregar el archivo a Package Info
+        package_info.files[package_info.file_count] = add_file;
+        package_info.file_count++;
+
+        // Escribir la información del archivo en el archivo empacado
+        // fwrite(&add_file, sizeof(struct File), 1, package);
+
+        // Copiar el contenido del archivo al archivo empacado
+        char buffer[1024];
+        size_t bytes_read;
+        while (bytes_read = fread(buffer, 1, sizeof(buffer), file) > 0) {
+            fwrite(buffer, 1, bytes_read, package);
+        }
+        
+        fclose(file);
+    }
+
+    // Vuelve al principio del archivo empacado para escribir la información de package_info
+    fseek(package, 0, SEEK_SET);
+    fwrite(&package_info, sizeof(struct PackageInfo), 1, package);
+    
+    fclose(package);
+
+    if(verbose_flag){
+        int numReports = sizeof(reports) / sizeof(reports[0]);
+        verbose(reports, numReports);
+    }
+}
+
+
+
+void recuperar_info(struct PackageInfo *empacado, const char* package_name){
     // Abrir el archivo en modo lectura binaria
     FILE *archivo = fopen(package_name, "rb");
 
@@ -53,7 +106,7 @@ void recuperar_info(struct Archive *empacado, const char* package_name){
     }
 
     // Leer la estructura desde el archivo
-    size_t leidos = fread(empacado, sizeof(struct Archive), 1, archivo);
+    size_t leidos = fread(empacado, sizeof(struct PackageInfo), 1, archivo);
 
     if (leidos != 1) {
         perror("Error al leer el archivo");
@@ -67,7 +120,7 @@ void recuperar_info(struct Archive *empacado, const char* package_name){
     // Imprimir los datos leídos
     printf("Tamaño de archivo: %lu\n", empacado->file_count);
 
-    // Ahora, puedes acceder a los datos de struct File dentro de struct Archive
+    // Ahora, puedes acceder a los datos de struct File dentro de struct package_info
     for (size_t i = 0; i < empacado->file_count; i++) {
         printf("Nombre de archivo: %s\n", empacado->files[i].name);
         printf("Tamaño: %lu\n", empacado->files[i].size);
@@ -80,20 +133,46 @@ void file(const char* package_name, char** files, int file_count){}
 
 void extraer(){}
 
-void listar(){}
+void listar(struct PackageInfo *empacado, const char* package_name){
+    // Abrir el archivo en modo lectura binaria
+    FILE *archivo = fopen(package_name, "rb");
+
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo para lectura");
+        return;
+    }
+
+    // Leer la estructura desde el archivo
+    size_t leidos = fread(empacado, sizeof(struct PackageInfo), 1, archivo);
+
+    if (leidos != 1) {
+        perror("Error al leer el archivo");
+        fclose(archivo);
+        return;
+    }
+
+    // Cerrar el archivo
+    fclose(archivo);
+
+
+    // Ahora, puedes acceder a los datos de struct File dentro de struct package_info
+    for (size_t i = 0; i < empacado->file_count; i++) {
+        printf("Nombre de archivo: %s\n", empacado->files[i].name);
+    }
+}
 
 void delete(){}
 
 void actualizar(){}
 
-void verbose(){}
+
 
 void agregar(){}
 
 void desfragmentar(){}
 
 int main(int argc, char *argv[]) {
-  /* if (argc < 3) {
+ if (argc < 3) {
         printf("Uso: %s <comando> <archivoEmpacado> [archivos...]\n", argv[0]);
         return 1;
     }
@@ -101,14 +180,15 @@ int main(int argc, char *argv[]) {
     const char* comando = argv[1];
     long int longitud_comando = strlen(comando)-1;
     const char* package_name = argv[2];
+    
     // argv[1][1]
     // printf("Comando: %c\n", comando[0]);
     // printf("Tam: %ld\n", longitud_comando);
-
+    const char* archivos = argv[3];
     for (int i = 1; i <= longitud_comando; i++){
         if (comando[i] == 'c'){
             printf("Opcion de comando: Create \n");
-            crear(package_name);
+            crear(package_name, &argv[3], argc - 3);
         }
         else if (comando[i] =='x'){
             printf("Opcion de comando: Extract \n");
@@ -124,6 +204,7 @@ int main(int argc, char *argv[]) {
         }
         else if(comando[i] == 'v'){
             printf("Opcion de comando: Verbose \n");
+            verbose_flag = true;
         }
         else if(comando[i] == 'f'){
             printf("Opcion de comando: File \n");
@@ -139,13 +220,13 @@ int main(int argc, char *argv[]) {
             printf("La letra ingresada no es valida");
         }
         
-    }*/
+    }
 
-    struct Archive archivo;
+    // struct PackageInfo archivo;
 
-    const char* nombre = "hola.jaja";
+    // const char* nombre = "hola.j2";
 
-    recuperar_info(&archivo, nombre);
+    // listar(&archivo, nombre);
 
 
     return 0;
